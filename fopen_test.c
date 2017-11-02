769c4d4db0509436df3ac4bf29fc412afee8cf3d
muguogang <muguogang@126.com>
2017-11-03 03:02:07 +0800

    add strerror()

--- fopen_test.c
+++ fopen_test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 int main(void)
 {
@@ -6,7 +8,8 @@ int main(void)
 	
 	if((fp = fopen("1.txt", "r+") ) == NULL)
 	{
-		perror("Failed to fopen 1.txt "); //输出错误信息
+		//perror("Failed to fopen 1.txt "); //输出错误信息
+		printf("failed to fopen: error_num = %d: %s\n", errno, strerror(errno) );
 		return -1;
 	}
 	printf("Open 1.txt successfully!\n");
